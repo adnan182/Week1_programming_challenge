d8dd7cdba27b744ccb4a0d96342fc4b0dd04d973
adnan182 <adnanhossain182@gmail.com>
2024-07-16 06:57:53 +0600

    python code updated

---
+++ 1.FizzBuzz/fizzbuzz.py
@@ -0,0 +1,18 @@
+def fizz_buzz(n):
+#range of numbers 1 to n
+    for i in range(1, n + 1):
+#If the number is divisible by both 3 and 5
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+#If the number is divisible by 3
+        elif i % 3 == 0:
+            print("Fizz")
+#If the number is divisible by 5
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+# Example usage
+n = 15
+fizz_buzz(n)
